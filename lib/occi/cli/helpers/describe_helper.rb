2d745abba1a8fdc3957b066ec6aa2730bd683fc2
Boris Parak <parak@cesnet.cz>
2013-12-03 23:30:56 +0100

    There is no #merge! for Occi::Core::Resources

--- lib/occi/cli/helpers/describe_helper.rb
+++ lib/occi/cli/helpers/describe_helper.rb
@@ -5,14 +5,14 @@ module Occi::Cli::Helpers::DescribeHelper
       Occi::Log.debug "#{options.resource} is a resource type or an actual resource."
 
       found = Occi::Core::Resources.new
-      found.merge! describe(options.resource)
+      found.merge describe(options.resource)
     elsif mixin_types.include? options.resource
       Occi::Log.debug "#{options.resourcre} is a mixin type."
 
       found = Occi::Core::Mixins.new
       mixins(options.resource).each do |mxn|
         mxn = mxn.split("#").last
-        found.merge! mixin(mxn, options.resource, true)
+        found.merge mixin(mxn, options.resource, true)
       end
     elsif mixin_types.include? options.resource.split('#').first
       Occi::Log.debug "#{options.resource} is a specific mixin type."
@@ -20,7 +20,7 @@ module Occi::Cli::Helpers::DescribeHelper
       mxn_type, mxn = options.resource.split('#')
 
       found = Occi::Core::Mixins.new
-      found.merge! mixin(mxn, mxn_type, true)
+      found.merge mixin(mxn, mxn_type, true)
     else
       Occi::Log.error "I have no idea what #{options.resource} is ..."
       raise "Unknown resource #{options.resource}, there is nothing to describe here!"
