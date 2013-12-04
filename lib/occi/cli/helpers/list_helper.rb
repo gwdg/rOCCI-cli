11f856e4d0f9d5747be77fd0eb2657a97c6abb57
Boris Parak <parak@cesnet.cz>
2013-12-04 15:12:30 +0100

    Adding support for identifiers to ListHelper

--- lib/occi/cli/helpers/list_helper.rb
+++ lib/occi/cli/helpers/list_helper.rb
@@ -3,10 +3,10 @@ module Occi::Cli::Helpers::ListHelper
   def helper_list(options, output = nil)
     found = []
 
-    if resource_types.include? options.resource
+    if resource_types.include?(options.resource) || resource_type_identifiers.include?(options.resource)
       Occi::Log.debug "#{options.resource} is a resource type."
       found = list options.resource
-    elsif mixin_types.include? options.resource
+    elsif mixin_types.include?(options.resource) || mixin_type_identifiers.include?(options.resource)
       Occi::Log.debug "#{options.resource} is a mixin type."
       found = mixin_list options.resource
     else
